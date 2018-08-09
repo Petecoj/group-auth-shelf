f012612119373c8e6c6c52a9c087b93c4111f61b
Grace Kasahara <gracekasahara@gmail.com>
2018-08-09 13:15:10 -0500

    added itemList reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,22 @@ import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
 
+
+const itemList = (state = [], action) => {
+  switch (action.type) {
+    case 'GET_LIST':
+      return state
+    case 'POST_ITEM':
+      return state
+    default:
+      return state;
+  }
+};
+
 const store = combineReducers({
   user,
   login,
+  itemList
 });
 
 export default store;
